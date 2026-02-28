0fa49cf995361e193ed6801b38c905da56c4b6f2
jmmd2002 <joaomirandamachado@gmail.com>
2026-02-28 23:23:52 +0100

    Python receives game state

--- godot_files/data/data_collector.gd
+++ godot_files/data/data_collector.gd
@@ -1,23 +1,63 @@
 extends Node
 
-var client : StreamPeerTCP = StreamPeerTCP.new()
+var client: StreamPeerTCP = StreamPeerTCP.new()
+var connected: bool = false
+var reconnect_timer: float = 0.0
+const RECONNECT_INTERVAL: float = 1.0  # seconds
 
-func _ready():
-	var err: Error = client.connect_to_host("127.0.0.1", 5000)
-	if err == OK:
-		print("Connected to Python server")
+#get game objects
+var ball: Node2D
+var paddleA: Node2D
+var paddleB: Node2D
+
+func _ready() -> void:
+	print("Connecting to server...")
+
+func _process(delta: float):
+	if not connected:
+		reconnect_timer +=  delta
+		if reconnect_timer >= RECONNECT_INTERVAL: #try to reconnect after a certain amount of time
+			reconnect_timer = 0
+			try_connect()
 	else:
-		print("Connection failed")
-
-func _process(delta):
-	if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
 		send_state()
 
-func send_state():
-	var state: Dictionary = {
-		"ball_y": randf(),
-		"paddle_y": randf()
-	}
+func try_connect():
+	var err: Error = client.connect_to_host("127.0.0.1", 5000)
+	if err == OK:
+		client.poll() #poll every step for server status
+		if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+			connected = true
+			print("Connected to Python server!")
+		else:
+			print("Connecting... still not ready")
+	else:
+		client = StreamPeerTCP.new()
+		print("Connection error with value ", err, ". Retrying...")
 
-	var json: String = JSON.stringify(state) + "\n"
-	client.put_data(json.to_utf8_buffer())
+func send_state():
+	
+	for b in get_tree().get_nodes_in_group("ball"):
+		ball = b #currently gets only first ball; needs fixing later
+		break
+	paddleA = get_tree().get_root().get_node_or_null("Game/PaddleA")
+	paddleB = get_tree().get_root().get_node_or_null("Game/PaddleB")
+	
+	#skip if any of the instances does not exist
+	if ball == null or paddleA == null or paddleB == null:
+		return
+	else:	
+		var state: Dictionary = {
+			"paddleA_x": paddleA.position.x,
+			"paddleA_y": paddleA.position.y,
+			"paddleB_x": paddleB.position.x,
+			"paddleB_y": paddleB.position.y,
+			"ball_x": ball.position.x,
+			"ball_y": ball.position.y,
+			"ball_vx": ball.velocity.x,
+			"ball_vy": ball.velocity.y
+		}
+		var json: String = JSON.stringify(state) + "\n"
+		client.put_data(json.to_utf8_buffer())
+		print("Sent:", json)
+		return
--- python_files/server.py
+++ python_files/server.py
@@ -4,7 +4,7 @@ HOST = "127.0.0.1"
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #make port reusable in case python breaks prematurely
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #make port reusable in case python breaks prematurely
 server.bind((HOST, PORT))
 server.listen(1)
 
@@ -12,13 +12,17 @@ print("Waiting for connection...")
 conn, addr = server.accept()
 print(f"Connected by {addr}")
 
+buffer = ""
 while True:
     data = conn.recv(1024)
     if not data:
         break
 
-    message = data.decode()
-    print("Received:", message)
+    buffer += data.decode()
+
+    while "\n" in buffer: #split messages on new linez
+        line, buffer = buffer.split("\n", 1)
+        print("Received:", line)
 
 #close server
 conn.close()
